8bc4f7828813e4b84dd4bc8f039a6ecfa7d3ffd5
Chelsea-Warren <warrenchelsea546@gmail.com>
2019-11-26 23:16:36 +0000

    Checked for ties

--- js/script.js
+++ js/script.js
@@ -8,17 +8,38 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+
+
 $("document").ready(function(){
     $("#shoot").click(function() {
         let val =$("input").val();
         $("#userChoice").text(val);
-    });
+
+        let index=Math.floor(Math.random() * 3);
+        console.log(index);
+        let computerChoice=["Rock","Paper","Scissor"];
+        computerChoice[index];
+         $("#computerChoice").text( computerChoice[index]);
+
+
+
+if(userChoice === "rock" && computerChoice=== "Rock" ||
+    userChoice=== "paper" && computerChoice=== "Paper" ||
+    userChoice=== "scissors" && computerChoice=== "Scissors"){
+       $("#result").html("Nobody Wins ");
+    }
+
+else if(userChoice === "rock" && computerChoice=== "Scissors" ||
+    userChoice=== "paper" && computerChoice=== "Rock" ||
+    userChoice=== "scissors" && computerChoice=== "Paper"){
+       $("#result").html("You Win ");
+    }
+
+
+
+
+
+
+
+    }); 
 });
-
-let index=Math.floor(Math.random() * 3);
-console.log(index);
-let computerChoice=["Rock","Paper","Scissor"];
-computerChoice[index];
-
-
-
