ddd0bb256b080199eca036614bec28b4b8cc1aac
Chelsea-Warren <warrenchelsea546@gmail.com>
2019-12-03 22:43:15 +0000

    background

--- js/script.js
+++ js/script.js
@@ -12,34 +12,50 @@
 
 $("document").ready(function(){
     $("#shoot").click(function() {
-        let val =$("input").val();
-        $("#userChoice").text(val);
+        let userChoice =$("input").val();
+    userChoice=userChoice.toUpperCase()
+        $("#userChoice").text(userChoice);
 
         let index=Math.floor(Math.random() * 3);
         console.log(index);
-        let computerChoice=["Rock","Paper","Scissor"];
-        computerChoice[index];
-         $("#computerChoice").text( computerChoice[index]);
+        let computerChoices=["Rock","Paper","Scissor"];
+        let computerChoice=computerChoices[index];
+        computerChoice=computerChoice.toUpperCase()
+         $("#computerChoice").text( computerChoice
+         );
 
-
+console.log("Computer Choice",computerChoice);
+console.log("Users Choice", userChoice);
 
 if(userChoice === "rock" && computerChoice=== "Rock" ||
     userChoice=== "paper" && computerChoice=== "Paper" ||
-    userChoice=== "scissors" && computerChoice=== "Scissors"){
+    userChoice=== "scissors" && computerChoice=== "Scissor"){
        $("#result").html("Nobody Wins ");
     }
 
-else if(userChoice === "rock" && computerChoice=== "Scissors" ||
+else if (userChoice === "rock" && computerChoice=== "Scissor" ||
     userChoice=== "paper" && computerChoice=== "Rock" ||
     userChoice=== "scissors" && computerChoice=== "Paper"){
        $("#result").html("You Win ");
     }
 
+    else if (userChoice === "rock" && computerChoice=== "Paper" ||
+    userChoice=== "paper" && computerChoice=== "Scissor" ||
+    userChoice=== "scissors" && computerChoice=== "Rock"){
+       $("#result").html("Computer Win ");
+    }
+
+else{
+$("#result").html("ERROR");
+}
+  }); //close click handler//
+
+$("#name").html("Chelsea ".repeat(5000));
 
 
 
 
 
 
-    }); 
+  
 });
